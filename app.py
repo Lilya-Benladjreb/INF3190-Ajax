aca7a36763375fc264c5fdf1db0b3c97f3c6b0ee
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2022-11-28 09:57:59 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/query", method=["GET"])
+def query():
+    query = request.args.get("q")
+    fichier = open("cours.txt", "r")
+    lignes = fichier.readlines()
+    coursTrouves = []
+    for ligne in lignes:
+        if query in ligne.lower():
+            coursTrouves.append(ligne)
+    return render_template("listeCours.html", liste=coursTrouves)
