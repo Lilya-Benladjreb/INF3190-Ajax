bf0461cefd96471baeb5555708a4e731c3e487aa
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2022-11-28 10:50:14 -0500

    added fetch

--- static/index.js
+++ static/index.js
@@ -6,7 +6,16 @@ function rechercherAvecXHR(){
             document.getElementById("table").innerHTML = xhr.responseText;
         }
     }
-    query = document.getElementById("recherche").value
-    xhr.open("GET", "/query?q=" + query, true)
-    xhr.send()
+    query = document.getElementById("recherche").value;
+    xhr.open("GET", "/query?q=" + query, true);
+    xhr.send();
+}
+
+async function rechercherAvecFetch() {
+    console.log("Fetch")
+    query = document.getElementById("recherche").value;
+
+    let f = await fetch("/query?q=" + query);
+    let text = await f.text();
+    document.getElementById("table").innerHTML = text;
 }
